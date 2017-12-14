a5d57cd04648078d32722ea1b47a5538c94d22f2
Liu Ray <manina7539@Lius-MacBook-Pro.local>
2017-12-14 18:57:42 +0800

    use synchronized keyword and lock object inside function: successfully reduces time cost

--- src/Worker.java
+++ src/Worker.java
@@ -6,42 +6,46 @@ public class Worker {
 	private List<Integer> list1 = new ArrayList<>();
 	private List<Integer> list2 = new ArrayList<>();
 	private Random random = new Random();
-
-	public synchronized void stageOne() {
-		try {
-			Thread.sleep(1);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	private Object lock1 = new Object();
+	private Object lock2 = new Object();
+	
+	public void stageOne() {
+		synchronized (lock1) { // it's a separate implementation, can use list1 as lock1, but not a better practice
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			list1.add(random.nextInt(100));
 		}
-		list1.add(random.nextInt(100));
-		
 	}
 
-	public synchronized void stageTwo() {
-		try {
-			Thread.sleep(1);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void stageTwo() {
+		synchronized (lock2) { // it's a separate implementation, can use list2 as lock2, but not a better practice
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			list2.add(random.nextInt(100));
 		}
-		list2.add(random.nextInt(100));
-		
 	}
 
 	public void process() {
-		for (int i=0;i<1000;i++) {
+		for (int i = 0; i < 1000; i++) {
 			stageOne();
 			stageTwo();
-			
+
 		}
-	} 
-	
+	}
+
 	public void main() {
 		System.out.println("Starting...");
-		
+
 		long start = System.currentTimeMillis();
-		
+
 		Thread t1 = new Thread(new Runnable() {
 			@Override
 			public void run() {
@@ -56,7 +60,7 @@ public class Worker {
 				process();
 			}
 		});
-		
+
 		t1.start();
 		t2.start();
 		try {
@@ -66,15 +70,15 @@ public class Worker {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		long end = System.currentTimeMillis();
-		
-		System.out.println("Time take: " + (end - start));
-		System.out.println("List1: " + list1.size() + "; List2: " +list2.size());
 
-// Output:		
+		long end = System.currentTimeMillis();
+
+		System.out.println("Time take: " + (end - start));
+		System.out.println("List1: " + list1.size() + "; List2: " + list2.size());
+
+// Output:
 //		Starting...
-//		Time take: 5359
+//		Time take: 2689
 //		List1: 2000; List2: 2000
 	}
 }
