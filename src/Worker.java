bd2eb5f294f0b5dffd9ab13ec89aa44f5465fb4f
Liu Ray <manina7539@Lius-MacBook-Pro.local>
2017-12-14 18:21:00 +0800

    worst case of time cost to do multithreading

--- src/Worker.java
+++ src/Worker.java
@@ -3,7 +3,55 @@ import java.util.List;
 import java.util.Random;
 
 public class Worker {
+	private List<Integer> list1 = new ArrayList<>();
+	private List<Integer> list2 = new ArrayList<>();
+	private Random random = new Random();
+
+	public void stageOne() {
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		list1.add(random.nextInt(100));
+		
+	}
+
+	public void stageTwo() {
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		list2.add(random.nextInt(100));
+		
+	}
+
+	public void process() {
+		for (int i=0;i<1000;i++) {
+			stageOne();
+			stageTwo();
+			
+		}
+	} 
+	
 	public void main() {
-		System.out.println("Hello");
+		System.out.println("Starting...");
+		
+		long start = System.currentTimeMillis();
+		
+		process();
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println("Time take: " + (end - start));
+		System.out.println("List1: " + list1.size() + "; List2: " +list2.size());
+
+// Output:		
+//		Starting...
+//		Time take: 2688
+//		List1: 1000; List2: 1000
 	}
 }
