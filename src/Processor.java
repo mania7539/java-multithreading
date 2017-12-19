df8ac2a75088866ae01a8f1bde86361786ca7679
mania7539 <mania7539@gmail.com>
2017-12-19 17:33:43 +0800

    producer-consumer pattern intial setup: testing environment

--- src/Processor.java
+++ src/Processor.java
@@ -1,9 +1,11 @@
+import java.util.LinkedList;
+import java.util.Random;
 
 public class Processor {
     public void produce() throws InterruptedException {
         
     }
-    
+
     public void consume() throws InterruptedException {
         
     }
