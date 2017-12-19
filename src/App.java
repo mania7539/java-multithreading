49084a019fb71d194535268450d4ac073d0fe422
mania7539 <mania7539@gmail.com>
2017-12-19 16:57:19 +0800

    producer-consumer pattern initial setup

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 
 public class App {
 	public static void main(String[] args) {
-		new Worker().main();
+		
 		
 	}
 }
---
+++ src/Processor.java
@@ -0,0 +1,10 @@
+
+public class Processor {
+    public void produce() throws InterruptedException {
+        
+    }
+    
+    public void consume() throws InterruptedException {
+        
+    }
+}
