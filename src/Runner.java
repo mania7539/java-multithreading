60080df221873d6a98fdffc89d7530bfecaa53fb
mania7539 <mania7539@gmail.com>
2017-12-19 18:07:11 +0800

    use ReentrantLock object with its functions lock and unlock to work as synchronized block pattern - no exception handling

--- src/Runner.java
+++ src/Runner.java
@@ -1,15 +1,34 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Runner {
+    private int count = 0;
+    private Lock lock = new ReentrantLock();
+    // once a thread acquires this lock (once a thread lock this lock),
+    //  it can lock again if it wants to (the same thread) and the lock just keeps count how many times it's been locked, 
+    //  and then you have to unlock it (the same thread) by the same number of times
+    //
+    // it works like synchronized block, but in some circumstances it has some advantages
+    //
+    private void increment() {
+        for (int a = 0; a < 10000; a++) {
+            count++;
+        }
+    }
 
     public void firstThread() throws InterruptedException {
-
+        lock.lock();
+        increment();
+        lock.unlock();  // not a good practice: unlock() will fail to call if increment() throws an exception
     }
 
     public void secondThread() throws InterruptedException {
-
+        lock.lock();
+        increment();
+        lock.unlock();  // not a good practice: unlock() will fail to call if increment() throws an exception
     }
 
     public void finished() {
-
+        System.out.println("Count is: " + count);
     }
 }
