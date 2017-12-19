561960e7fafe070f13ec40e75c84b092f74d9e6d
mania7539 <mania7539@gmail.com>
2017-12-19 17:46:09 +0800

    re-entrant lock: initial setup

--- src/App.java
+++ src/App.java
@@ -1,7 +1,36 @@
 
 public class App {
-	public static void main(String[] args) {
-		new Worker().main();
+	public static void main(String[] args) throws InterruptedException {
+		final Runner runner = new Runner();
 		
+		Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    runner.firstThread();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+		
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    runner.secondThread();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+		
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
+		
+		runner.finished();
 	}
 }
---
+++ src/Runner.java
@@ -0,0 +1,15 @@
+
+public class Runner {
+
+    public void firstThread() throws InterruptedException {
+
+    }
+
+    public void secondThread() throws InterruptedException {
+
+    }
+
+    public void finished() {
+
+    }
+}
