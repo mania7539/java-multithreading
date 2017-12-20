285cf0a6c7dbff344e42d870e9b968132e920456
mania7539 <mania7539@gmail.com>
2017-12-20 12:05:31 +0800

    add account1, account2 to Runner class for initial testing

--- src/Runner.java
+++ src/Runner.java
@@ -1,14 +1,24 @@
 public class Runner {
-
+    private Account account1 = new Account();
+    private Account account2 = new Account();
+    
     public void firstThread() throws InterruptedException {
+        
     }
 
     public void secondThread() throws InterruptedException {
+        
     }
 
     public void finished() {
+        System.out.println("Account 1 balance: " + account1.getBalance());
+        System.out.println("Account 2 balance: " + account2.getBalance());
+        
+        System.out.println("Total balance: " + (account1.getBalance() + account2.getBalance()));
     }
     
 // Output:
-
+//    Account 1 balance: 10000
+//    Account 2 balance: 10000
+//    Total balance: 20000
 }
