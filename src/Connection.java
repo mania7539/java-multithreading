1bfb57302a119c9319de83cb50753c78d142c336
David.Rz.Liu <mania7539@gmail.com>
2017-12-21 00:42:06 +0800

    use semaphore to limit the maximum connections to 10 by adding Semaphore objects with 10 permits and using acquire() and release() before and after the original connect() codes

--- src/Connection.java
+++ src/Connection.java
@@ -1,7 +1,9 @@
+import java.util.concurrent.Semaphore;
 
 public class Connection {
     private static Connection instance = new Connection();
     private int connections = 0;
+    private Semaphore semaphore = new Semaphore(10);
     
     private Connection() {
         
@@ -12,6 +14,12 @@ public class Connection {
     }
     
     public void connect() {
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+        
         synchronized (this) {
             connections++;
             System.out.println("Current connections: " + connections);
@@ -26,6 +34,8 @@ public class Connection {
         synchronized (this) {
             connections--;
         }
+        
+        semaphore.release();
     }
     
 // Output:
@@ -39,28 +49,38 @@ public class Connection {
 //    Current connections: 8
 //    Current connections: 9
 //    Current connections: 10
-//    Current connections: 11
-//    Current connections: 12
-//    Current connections: 13
+//    Current connections: 1
+//    Current connections: 2
+//    Current connections: 3
+//    Current connections: 4
+//    Current connections: 5
+//    Current connections: 6
+//    Current connections: 7
+//    Current connections: 8
+//    Current connections: 9
+//    Current connections: 10
+//    Current connections: 7
+//    Current connections: 8
+//    Current connections: 9
+//    Current connections: 10
+//    Current connections: 5
+//    Current connections: 6
+//    Current connections: 7
+//    Current connections: 8
+//    Current connections: 9
+//    Current connections: 10
 //    ...
-//    Current connections: 181
-//    Current connections: 182
-//    Current connections: 183
-//    Current connections: 184
-//    Current connections: 185
-//    Current connections: 186
-//    Current connections: 187
-//    Current connections: 188
-//    Current connections: 189
-//    Current connections: 190
-//    Current connections: 191
-//    Current connections: 192
-//    Current connections: 193
-//    Current connections: 194
-//    Current connections: 195
-//    Current connections: 196
-//    Current connections: 197
-//    Current connections: 198
-//    Current connections: 199
-//    Current connections: 200
+//    Current connections: 8
+//    Current connections: 9
+//    Current connections: 10
+//    Current connections: 8
+//    Current connections: 9
+//    Current connections: 10
+//    Current connections: 10
+//    Current connections: 8
+//    Current connections: 9
+//    Current connections: 10
+//    Current connections: 8
+//    Current connections: 9
+//    Current connections: 10
 }
