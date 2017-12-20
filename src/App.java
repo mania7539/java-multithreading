160c5fd469968cb84f7d935ce3e8ff7e577ed87e
David.Rz.Liu <mania7539@gmail.com>
2017-12-21 00:21:41 +0800

    semaphore object intro and setup

--- src/App.java
+++ src/App.java
@@ -1,7 +1,17 @@
+import java.util.concurrent.Semaphore;
 
 public class App {
-	public static void main(String[] args) {
-		new Worker().main();
+	public static void main(String[] args) throws InterruptedException {
+		Semaphore semaphore = new Semaphore(1);   // use 0 or 1 for permits would be reasonable
 		
+		semaphore.release();  // semaphore.availablePermits() +1
+		semaphore.acquire();  // semaphore.availablePermits() -1
+		// If semaphore.availablePermits() == 0, acquire() would wait until a permit is released
+		
+		System.out.println("Available permits: " + semaphore.availablePermits());
 	}
+	
+// Output:
+//	Available permits: 1
+
 }
