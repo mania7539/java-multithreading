99092d1ef6ad3d2a76cca94c74b9e5dddcf04614
mania7539 <mania7539@gmail.com>
2017-12-20 12:14:15 +0800

    solve the money-transferred problem: use ReentrantLock and its functions lock and unlock with try, finally

--- src/Runner.java
+++ src/Runner.java
@@ -1,14 +1,26 @@
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Runner {
     private Account account1 = new Account();
     private Account account2 = new Account();
 
+    private Lock lock1 = new ReentrantLock();
+    private Lock lock2 = new ReentrantLock();
+    
     public void firstThread() throws InterruptedException {
         Random random = new Random();
 
         for (int a = 0; a < 10000; a++) {
-            Account.transfer(account1, account2, random.nextInt(100));
+            lock1.lock();
+            lock2.lock();
+            try {
+                Account.transfer(account1, account2, random.nextInt(100));                
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
         }
     }
 
@@ -16,7 +28,14 @@ public class Runner {
         Random random = new Random();
 
         for (int a = 0; a < 10000; a++) {
-            Account.transfer(account2, account1, random.nextInt(100));
+            lock1.lock();
+            lock2.lock();
+            try {
+                Account.transfer(account2, account1, random.nextInt(100));                
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
         }
     }
 
@@ -28,7 +47,7 @@ public class Runner {
     }
 
 // Output:
-//    Account 1 balance: 3462
-//    Account 2 balance: 17786
-//    Total balance: 21248
+//    Account 1 balance: 8079
+//    Account 2 balance: 11921
+//    Total balance: 20000
 }
