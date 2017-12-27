7f41d3b9d8db8e6e2d1f5bdca299b48daf39e6d7
David.Rz.Liu <mania7539@gmail.com>
2017-12-28 00:11:05 +0800

    use Future and Callable<Integer> to store the Executors' task result in an async but clean way

--- src/App.java
+++ src/App.java
@@ -1,34 +1,52 @@
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class App {
 	public static void main(String[] args) {
 		ExecutorService executor = Executors.newCachedThreadPool();
-		executor.submit(new Runnable() {
-		    public void run() {
-		        Random random = new Random();
-		        int duration = random.nextInt(4000);
-		        
-		        System.out.println("Starting...");
-		        
-		        try {
+		
+		// we want to get the time (Integer) as the thread.sleep returns
+		    // you can also use ArrayList<Future<Integer>> to store the entire submit() results
+		Future<Integer> future = executor.submit(new Callable<Integer>() {
+
+            @Override
+            public Integer call() throws Exception {
+                Random random = new Random();
+                int duration = random.nextInt(4000);
+                
+                System.out.println("Starting...");
+                
+                try {
                     Thread.sleep(duration);
-                    // Question: how to get the exact system sleep time in a clean way?
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-		        
+                
 
-		        System.out.println("Finished.");
-		    }
+                System.out.println("Finished.");
+                return duration;    
+            }
 		    
 		});
 		
 		executor.shutdown();
+		try {
+            System.out.println("Result is: " + future.get());
+            // future.get() will still block until the result is on, if you don't make executor to wait
+            
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 	}
 	
 // output:
 //	Starting...
 //	Finished.
+//	Result is: 581
 }
