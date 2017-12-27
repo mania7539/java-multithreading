8d650974a65e8239ab335e83b54863bfeb46955d
David.Rz.Liu <mania7539@gmail.com>
2017-12-27 23:56:47 +0800

    address issue: how to get the exact system sleep time in a clean way?

--- src/App.java
+++ src/App.java
@@ -1,7 +1,34 @@
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class App {
 	public static void main(String[] args) {
-		new Worker().main();
+		ExecutorService executor = Executors.newCachedThreadPool();
+		executor.submit(new Runnable() {
+		    public void run() {
+		        Random random = new Random();
+		        int duration = random.nextInt(4000);
+		        
+		        System.out.println("Starting...");
+		        
+		        try {
+                    Thread.sleep(duration);
+                    // Question: how to get the exact system sleep time in a clean way?
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+		        
+
+		        System.out.println("Finished.");
+		    }
+		    
+		});
 		
+		executor.shutdown();
 	}
+	
+// output:
+//	Starting...
+//	Finished.
 }
